c00d9ade88301f9770e73eb71a487ee8bb511ebb
teknogeek <jmargolis97@gmail.com>
2014-06-26 12:06:16 -0400

    work on multiple coin sections

--- coinking/MasterViewController.m
+++ coinking/MasterViewController.m
@@ -32,12 +32,9 @@
 	// Do any additional setup after loading the view, typically from a nib.
 
     NSURL *url=[NSURL URLWithString:@"https://coinking.io/api.php?key=9f5671cf0a4bc9007fd9aa81a23e8c24&type=currentlymining&output=json"];   // pass your URL  Here.
-    
     NSData *data = [NSData dataWithContentsOfURL:url];
     NSError *error;
-    
     NSMutableDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-    
     NSLog(@"%@", json);
     
     NSMutableArray *nowMiningArray = [[NSMutableArray alloc] init];
@@ -46,8 +43,12 @@
     {
         [nowMiningArray addObject:restaurantParameters];
     }
-    NSLog(@"%@", [nowMiningArray[0] objectForKey:@"difficulty"]);
-    [self insertNewObject:[nowMiningArray[0] objectForKey:@"difficulty"]];
+    [self insertNewObject:@"Now Mining" value:[[nowMiningArray[1] objectForKey:@"name"] capitalizedString]];
+    [self insertNewObject:@"Difficulty" value:[nowMiningArray[0] objectForKey:@"difficulty"]];
+    [self insertNewObject:@"Difficulty" value:[nowMiningArray[0] objectForKey:@"difficulty"]];
+    [self insertNewObject:@"Difficulty" value:[nowMiningArray[0] objectForKey:@"difficulty"]];
+    [self insertNewObject:@"Hashrate (MH/s)" value:[nowMiningArray[0] objectForKey:@"hashrate"]];
+    [self insertNewObject:@"Now Mining" value:[[nowMiningArray[0] objectForKey:@"name"] capitalizedString]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -56,14 +57,21 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(NSString *)property
+- (void)insertNewObject:(NSString *)name value:(NSString *)value
 {
     if (!_objects)
     {
         _objects = [[NSMutableArray alloc] init];
     }
-    [_objects insertObject:property atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    NSString *result = [NSString stringWithFormat:@"%@: %@", name, value];
+    [_objects insertObject:result atIndex:0];
+    NSLog(@"%d", _objects.count);
+    NSInteger section = 0;
+    if(_objects.count == 4)
+    {
+        section = 2;
+    }
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:section];
     [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
@@ -71,7 +79,7 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -82,24 +90,26 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-
+    NSString *name = @"dogecoin";
     NSDate *object = _objects[indexPath.row];
     cell.textLabel.text = [object description];
     
+    if((indexPath.row == 0) && (indexPath.row == 6))
+    {
         UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
         UIImage *coinImage = [UIImage imageWithData:
-                          [NSData dataWithContentsOfURL:
-                           [NSURL URLWithString: @"https://coinking.io/assets/coins/large-dogecoin.png"]]];
+                              [NSData dataWithContentsOfURL:
+                               [NSURL URLWithString:[NSString stringWithFormat:@"https://coinking.io/assets/coins/large-%@.png", name]]]];
         imgView.image = coinImage;
         cell.imageView.image = imgView.image;
-    
+    }
     return cell;
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return NO;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
